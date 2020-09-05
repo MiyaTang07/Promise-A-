bc2edd316d67397cc072f74d126297e24dae984b
唐娇 <tangjiao@tangjiaodeMacBook-Air.local>
2020-09-05 14:29:39 +0800

    add more commits

--- promise.origin.js
+++ promise.origin.js
@@ -34,10 +34,7 @@ function rejectedPromise(promise, reason) {
 }
 
 function isFunction(fn) {
-  return (
-    Object.prototype.toString.call(fn).toLocaleLowerCase() ===
-    "[object function]"
-  );
+  return Object.prototype.toString.call(fn).toLocaleLowerCase() === "[object function]"
 }
 
 // 执行promise的callbacks
@@ -49,23 +46,20 @@ function isPromise(p) {
   return p instanceof Promise;
 }
 function isObject(obj) {
-  return (
-    Object.prototype.toString.call(obj).toLocaleLowerCase() ===
-    "[object object]"
-  );
+  return Object.prototype.toString.call(obj).toLocaleLowerCase() === "[object object]"
 }
 /***
  * promise的解析过程
  * 抽象模型[[Resolve]](promise, x)
- * 如果promise和x指向相同的值
- * 如果x是一个promise
- * 如果x是一个对象或一个函数
- * 如果x不是对象也不是函数
+ * 2.3.1 如果promise和x指向相同的值
+ * 2.3.2 如果x是一个promise
+ * 2.3.3 如果x是一个对象或一个函数
+ * 2.3.4 如果x不是对象也不是函数
  */
 function resolvePromise(promise, x) {
   // x 与promise相同
   if (promise === x) {
-    rejectedPromise(promise, new TypeError("cant be the same"));
+    rejectedPromise(promise, new TypeError("can not be the same"));
     return;
   }
   // x 是一个promise
@@ -106,7 +100,7 @@ function resolvePromise(promise, x) {
     if (isFunction(then)) {
       try {
         then.call(
-          x,
+           x,
           (y) => {
             // 如果被called，就返回
             if (called) {
@@ -124,6 +118,7 @@ function resolvePromise(promise, x) {
           }
         );
       } catch (error) {
+        // resolvePromise或者rejectPromise已经被调用了，则忽略它。
         if (called) {
           return;
         }
@@ -132,6 +127,7 @@ function resolvePromise(promise, x) {
       }
       return;
     } else {
+      // then不是一个函数，以x为值fulfill promise 
       fulfilledPromise(promise, x);
       return;
     }
@@ -148,38 +144,41 @@ class Promise {
     this.status = statusMap.PENDING;
     this.value = undefined;
     this.reason = undefined;
-    this.fulfilledCbs = []; // then的fulfilled callback
-    this.rejectedCbs = []; // then的rejected callback
+    this.fulfilledCbs = []; // then的fulfilled callbacks
+    this.rejectedCbs = []; // then的rejected callbacks
     fn(
       (value) => {
-        // fulfilledPromise(this, value);
-         resolvePromise(this, value)
+        fulfilledPromise(this, value);
+        //  resolvePromise(this, value)
       },
       (reason) => {
-        rejectedPromise(this, reason);
+         rejectedPromise(this, reason);
       }
     );
   }
   // 两个参数
   then(onFulfilled, onRejected) {
+    // 当前promise执行后生成新的promise
     const promise1 = this;
     const promise2 = new Promise(() => {});
-                          
+
+    // 根据当前promise状态判断then方法的执行                
     if (promise1.status === statusMap.FULFILLED) {
       if (!isFunction(onFulfilled)) {
         return promise1;
       }
+      // then方法里面异步执行，并且异常兼容
       setTimeout(() => {
         try {
           const x = onFulfilled(promise1.value);
-          // 根据x来设定promise的状态
+          // 根据x的值来设定新promise的状态，即决议promise2
           resolvePromise(promise2, x);
         } catch (error) {
           rejectedPromise(promise2, error);
         }
       }, 0);
     }
-
+    // 与fulfilled情况类似
     if (promise1.status === statusMap.REJECTED) {
       if (!isFunction(onRejected)) {
         return promise1;
@@ -193,18 +192,16 @@ class Promise {
         }
       }, 0);
     }
-
+    
+    // promise1未决议状态，需要将callbacks存储，并且按顺序执行
     if (promise1.status === statusMap.PENDING) {
       // promise的决议值支持透传
       onFulfilled = isFunction(onFulfilled) ? onFulfilled : (value) => value;
-      onRejected = isFunction(onRejected)
-        ? onRejected
-        : (err) => {
-            throw err;
-          };
+      onRejected = isFunction(onRejected) ? onRejected : (err) => { throw err; };
 
       // 需要用一个function包起来，因为需要将promise1的决议传入参数中
       promise1.fulfilledCbs.push(() => {
+        // then方法里面的callbacks仍然需要异步去执行
         setTimeout(() => {
           try {
             const x = onFulfilled(promise1.value);
@@ -214,6 +211,7 @@ class Promise {
           }
         }, 0);
       });
+
       promise1.rejectedCbs.push(() => {
         setTimeout(() => {
           try {
